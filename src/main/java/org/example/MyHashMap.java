b42dc32a3a52ca810fdf942a171507cf129eb88e
hojunking96 <hojunking96@gmail.com>
2023-04-20 09:13:51 +0900

    MyHashMap 초기설정

---
+++ src/main/java/org/example/MyHashMap.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+public class MyHashMap<K, V> {
+    public void put();
+
+    public void replace();
+
+    public void get();
+
+    public void remove();
+
+    public void size();
+
+    public void containsKey();
+
+    public void containsValue();
+
+    public void clear();
+
+    public void isEmpty();
+
+}
